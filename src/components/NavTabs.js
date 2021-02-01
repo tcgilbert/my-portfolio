26f5e267f9ead4f858436cf7ec7983bf863cb461
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-01 13:48:01 -0500

    [NavTabs] added buttons for navtabs

---
+++ src/components/NavTabs.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavTabs = () => {
+    return (
+        <div className="nav-tabs">
+            <button className="nav-btn">About</button>
+            <button className="nav-btn">Projects</button>
+            <button className="nav-btn">Contact</button>
+        </div>
+    )
+}
+
+export default NavTabs
