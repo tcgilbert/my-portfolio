e5856c01c22771c78875b60e0754db6ae954e24f
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-04 11:56:11 -0500

    [Navigation] added contact link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -13,6 +13,7 @@ const Navigation = () => {
         <div className="nav-tab-container">
             <h3 className="nav-tab">About</h3>
             <h3 className="nav-tab">Projects</h3>
+            <h3 className="nav-tab">Contact</h3>
             <h3 className="nav-tab">Resume</h3>
         </div>
 
