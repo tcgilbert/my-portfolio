e7568dc33c470b7abc6ffb63ab17d19ea039362e
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-08 21:58:53 -0500

    [Projects] added links to projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 const Projects = () => {
+
+
+    const kscopeRedirect = () => {
+        window.open("https://kaleidoscope-budget.herokuapp.com/", "_blank")
+    }
+
+    const thinkeweRedirect = () => {
+        window.open("https://think-ewe.herokuapp.com/", "_blank")
+    }
+
+    const sauceRedirect = () => {
+        window.open("https://tcgilbert.github.io/rocket-sauce/", "_blank")
+    }
+
     return (
         <div className="projects-section">
             <div className="projects-container">
@@ -8,15 +22,15 @@ const Projects = () => {
                     <h1 className="projects-heading">Featured Projects</h1>
                     <hr className="about-hr"/>
                 </div>
-                <div className="kaleidoscope" data-sal-duration="700" data-sal="slide-up">
+                <div onClick={kscopeRedirect} className="kaleidoscope" data-sal-duration="700" data-sal="slide-up">
                     <h1 className="project-title">Kaleidoscope</h1>
                     <p className="project-text">React | Node | MongoDB | Express </p>
                 </div>
-                <div className="think-ewe" data-sal-duration="700" data-sal="slide-up">
+                <div onClick={thinkeweRedirect} className="think-ewe" data-sal-duration="700" data-sal="slide-up">
                     <h1 className="project-title">Think Ewe</h1>
                     <p className="project-text">React | Node | PostgreSQL | Express </p>
                 </div>
-                <div className="rocket-sauce" data-sal-duration="700" data-sal="slide-up">
+                <div onClick={sauceRedirect} className="rocket-sauce" data-sal-duration="700" data-sal="slide-up">
                     <h1 className="project-title">Rocket Sauce</h1>
                     <p className="project-text">Vanilla JS | HTML5 Canvas | Adobe PS</p>
                 </div>
