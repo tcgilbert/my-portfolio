c681571d85f10bb7aeaf26e1bc8d01975e5a11ae
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-09 21:33:38 -0500

    new favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,18 @@ module.exports = {
           enterEventName: 'sal:in', // Enter event name
           exitEventName: 'sal:out', // Exit event name
       }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'React go',
+        short_name: 'Reactgo',
+        start_url: '/',
+        background_color: '#f7f0eb',
+        theme_color: '#a2466c',
+        display: 'standalone',
+        icon: 'static/favicon2.ico',
+      },
     }
   ]
 }
