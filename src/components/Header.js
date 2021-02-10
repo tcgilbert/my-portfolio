8cdfd4708a99b435cdaf696a200c7443475b44a0
Thomas Gilbert <tcgilbert94@gmail.com>
2021-02-09 19:40:46 -0500

    [Header] removed material ui icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,21 @@
 import React from "react"
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-const Header = (props) => {
-
+const Header = props => {
   function handleArrowClick() {
-    window.location.href="#about-anchor"
+    window.location.href = "#about-anchor"
   }
-
 
   return (
     <div className="header">
-
       <div className="title-container">
         <h1 className="title">Thomas Gilbert</h1>
         <h1 className="subtitle">Web Developer</h1>
-      <div className="arrow-btn" onMouseDown={handleArrowClick} >
-        <ExpandMoreIcon style={{ fontSize: 65, color: "white" }}/>
+        <div className="arrow-btn" onMouseDown={handleArrowClick}>
+          <p className="down-arrow">{"\u032c"}</p>
+        </div>
       </div>
-      </div>
-      
-
     </div>
   )
 }
 
 export default Header
-
